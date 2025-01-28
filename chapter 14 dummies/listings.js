36f471b63dc6c283dc9560768cea28532912816a
Ronny2001789 <ronaldmputla25@gmail.com>
2025-01-28 10:59:20 +0200

    update

---
+++ chapter 14 dummies/listings.js
@@ -0,0 +1,35 @@
+// Listing 14 - 1
+//var language = prompt("What language do you speak?");
+
+//if (language === "JavaScript") {
+  //  alert("Great! Let's talk JavaScript!");
+//} else {
+  //  alert("I don't know what you're saying.");
+//}
+
+// // Listing 14 - 2
+// var language = prompt("What language do you speak?");
+
+// if (language === "JavaScript") {
+//  alert("Great! Let's talk JavaScript!");
+//  var speaksJavaScript = true;
+// } else {
+//  alert("I don't know what you're saying.");
+// }
+
+// if (speaksJavaScript) {
+//  alert("It's great to meet you.");
+// }
+
+ let age  = prompt("how old are you");
+ let myname = prompt("enter your full name");
+ 
+ if(age == 40 && myname === "ronny"){
+    alert("welcome dude.")
+ } else {
+    alert("wrong infor")
+ }
+
+
+ 
+
---
+++ my web page/main.js
@@ -0,0 +1,17 @@
+let name = prompt("who are you");
+let number =prompt("enter your password");
+
+if(name = "ronny" , number = 23)
+    alert("welcome")
+else("you are not allowed")
+
+function countUp() {
+     for (let i = 0; i < 10; i++) {
+        alert("your order is pending");
+    }
+}
+     
+
+
+
+    
