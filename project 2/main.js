7a4fe6103fbaa231d4e4305b99ea98f741a59a78
Ronny2001789 <ronaldmputla25@gmail.com>
2025-01-29 16:37:28 +0200

    date

--- project 2/main.js
+++ project 2/main.js
@@ -1,11 +1,10 @@
-// Generate a random secret number between 1 and 20//
-let secret = Math.floor(Math.random() * 20) + 1;
+//produe a random secret number between 1 and 20
+let secret = Math.floor(Math.random() * 20) - 1; 
 let guess;
 
+while (guess !== secret) {
 
-
-  // Prompt the user for their guess and convert to an integer//
-
+  // Prompt the user for their guess and convert it to an integer
   guess = parseInt(prompt("Please guess the secret number (1-20) " + secret));
 
   if (guess === secret) {
@@ -15,10 +14,4 @@ let guess;
   } else if (guess > secret) {
     alert("Incorrect, too high.");
   }
- while (guess !== secret);
-
-
-
-
-
- 
+} 
